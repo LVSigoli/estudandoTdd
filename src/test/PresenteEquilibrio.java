126d48af523cc47c68d84401454ea7a53164f7e4
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2024-04-15 23:15:28 -0300

    Implementa lógica para verificar equilíbrio possível

---
+++ src/test/PresenteEquilibrio.java
@@ -0,0 +1,23 @@
+package test;
+
+public class PresenteEquilibrio {
+
+    public static String verificarPossibilidade(int[] pesos) {
+        int somaA = 0;
+        int somaB = 0;
+
+        for (int peso : pesos) {
+            if (Math.abs(somaA - somaB) > 5) {
+                return "N";
+            }
+
+            if (somaA <= somaB) {
+                somaA += peso;
+            } else {
+                somaB += peso;
+            }
+        }
+
+        return "S";
+    }
+}
